31df8d4f165074de3753eba036f8a4cc8c15be2d
Jm9710 <85179035+Jm9710@users.noreply.github.com>
2024-09-21 00:21:13 +0000

    generador de dominios finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateDomain() {
+    console.log("Hello Rigo from the console!");
+
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let ext = [".com", ".es", ".edu", ".uy"];
+
+    let domain =
+      pronoun[Math.floor(Math.random() * pronoun.length)] +
+      adj[Math.floor(Math.random() * adj.length)] +
+      noun[Math.floor(Math.random() * noun.length)] +
+      ext[Math.floor(Math.random() * ext.length)];
+
+    document.getElementById("dominio").innerHTML = domain;
+  }
+
+  let reload = document.getElementById("refresh");
+
+  reload.addEventListener("click", () => {
+    location.reload();
+  });
+
+  generateDomain();
 };
